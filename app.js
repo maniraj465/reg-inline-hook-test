c231b21a6b40299c64340c29fe7d3ffda06c7ed2
Maniraj Sivasubbu-PWC <maniraj.sivasubbu-pwc@hpe.com>
2021-06-05 14:32:18 +0530

    inline

--- app.js
+++ app.js
@@ -4,13 +4,27 @@ const app = express();
 app.use(express.json());
 
 app.post('/reg', (req, res) => {
-    let body = req.body;
-    let profile = body.profile;
-    profile.hpeProfileId = 123;
+    // let body = req.body;
+    // let profile = body.profile;
+    // profile.hpeProfileId = 123;
     
+    // let body = req.body;
+    // let profile = body.profile;
+    let id = 12345
     let response = {
-        profile: profile,
-    }
+        "commands":[
+           {
+              "type":"com.okta.user.pre-registration",
+              "value":{
+                 "firstName": req.data.userProfile.firstName,
+                 "lastName": req.data.userProfile.lastName,
+                 "email": req.data.userProfile.email,
+                 "password": req.data.userProfile.password,
+                 "hpeProfileId": id
+              }
+           }
+        ]
+     }
     
     console.log(response);
     res.send(response);
