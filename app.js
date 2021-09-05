dacba3bb23eb5d848e30fcd53bfa477e62f361c4
maniraj465 <smaniraj15@gmail.com>
2021-09-05 23:14:21 +0530

    latest code

--- app.js
+++ app.js
@@ -1,37 +1,118 @@
 const express = require('express');
-const {v4:uuid} = require('uuid');
+const axios = require('axios');
+
+const API_GATE_WAY_URL = 'https://0vv671dgs9.execute-api.us-east-1.amazonaws.com/test?TID=';
+const GET_USER_BY_UID = 'api/v1/users?search=profile.uid+eq+';
+const OKTA_BASE_URL = 'https://oie-3751727-admin.oktapreview.com/';
+const API_TOKEN = 'SSWS 00TnKVnUZ0eziDta_OgvZJGK7mpChMSnQni_bL9K15';
+let targetUrl;
 
 const app = express();
-
 app.use(express.json());
 
-app.post('/reg', (req, res) => {
-    
-    let response = {
-        "commands":[
-           {
-              "type":"com.okta.user.profile.update",
-              "value":{
-                 "firstName": req.body.data.userProfile.firstName,
-                "lastName": req.body.data.userProfile.lastName,
-                "email": req.body.data.userProfile.email,
-                "countryCode": req.body.data.userProfile.countryCode,	    
-                "hpeProfileID": uuid()
-              }
-           },
-        ]
-     }
-    
-    res.send(response);
+app.post('/register', async (req, res) => {
+    const requestBody = req.body;
+    const id = req.query.id;
+    const dynamoDBTargetUrl = API_GATE_WAY_URL + id;
+    const user = await getUserAWS(dynamoDBTargetUrl);
+
+    // {
+    //     "Count":1,
+    //     "Items":[{
+    //         "Address":{"S":"Bangalore"},
+    //         "First_Name":{"S":"Gautam"},
+    //         "Mobile":{"S":"1234567890"},
+    //         "Experience":{"S":"10"},
+    //         "Specialization":{"S":"Cyber Security"},
+    //         "Last_Name":{"S":"Chatterjee"},
+    //         "LatestDergee":{"S":"M.TECH"},
+    //         "Position":{"S":"Professor"},
+    //         "Email":{"S":"gautamkumarchatterjee@gmail.com"},
+    //         "TID":{"S":"TID001"
+    //         }
+    //     }],
+    //     "ScannedCount":1
+    // }
+
+    // {
+    //     "LatestDegree":"M.TEch",
+    //     "Specialization":"Cyber",
+    //     "name": null,
+    //     "email": null,
+    //     "given_name":null,
+    //     "family_name": null,
+    //     "mobilePhone": "5454",
+    //     "experience":"10",
+    //     "postalAddress":"Bangalore"
+    // }
+
+    const userProfile = user.Items[0];
+    console.log(userProfile);
+    const oktaTargetUrl = `${OKTA_BASE_URL}${GET_USER_BY_UID}"${id}"`;
+    const oktaData = await getUserOKTA(oktaTargetUrl);
+    const payload = {
+        profile: {
+            "firstName": userProfile.First_Name.S,
+            "lastName": userProfile.Last_Name.S,
+            "email": userProfile.Email.S,
+            "mobilePhone": userProfile.Mobile.S,
+            "uid": userProfile.TID.S,
+            "experience": userProfile.Experience.S,
+            "postalAddress": userProfile.Address.S,
+            "specialization": userProfile.Specialization.S,
+            "latestDegree": userProfile.LatestDergee.S,
+        }
+    };
+    const updateResponse = updateUserOKTA(targetUrl, payload)
+    res.send(updateResponse);
 });
-app.get('/', (req, res) => {
-    response = {
-        "name": "maniraj",
-        "role": "developer"
-    }
-    console.log(response);
-    res.send(response);
-});
+
+async function getUserAWS(targetUrl) {
+    const response = await axios.get(targetUrl)
+    .then(res => {
+        return res.data;
+    })
+    .catch(error => {
+        return error;
+    });
+    return response;
+}
+
+async function getUserOKTA(targetUrl) {
+    const response = await axios.get(targetUrl, {
+        headers: {
+            'Content_Type': 'application/json',
+            Accept:  'application/json',
+            Authorization: API_TOKEN
+        }
+    })
+    .then(res => {
+        return res.data;
+    })
+    .catch(error => {
+        console.log(error);
+        return error;
+    });
+    return await response;
+}
+
+async function updateUserOKTA(targetUrl, payload) {
+    const response = await axios.put(targetUrl, payload, {
+        headers: {
+            'Content_Type': 'application/json',
+            Accept:  'application/json',
+            Authorization: API_TOKEN
+        }
+    })
+    .then(res => {
+        return res.data;
+    })
+    .catch(error => {
+        console.log(error);
+        return error;
+    });
+    return await response;
+}
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
