438d521785f46715291ad3a2c26ad6dd6a31f811
maniraj-sivasubbu-pwc <maniraj.sivasubbu-pwc@hpe.com>
2021-10-01 22:19:54 +0530

    v

--- app.js
+++ app.js
@@ -173,10 +173,10 @@ app.patch('/scim/v2/Users/:profileId', (req, res) => {
 
 
 app.put("/scim/v2/Users/:userId", function (req, res) {
-	var userId = req.params.userId;
-	var url_parts = url.parse(req.url, true);
-	var req_url = url_parts.pathname;
-  var requestBody = "";
+// 	var userId = req.params.userId;
+// 	var url_parts = url.parse(req.url, true);
+// 	var req_url = url_parts.pathname;
+//   var requestBody = "";
 	
   req.on('data', function (data) {
     requestBody += data;
