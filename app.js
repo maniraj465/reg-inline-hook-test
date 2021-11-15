171f559dbf7778b86798322f2d4a445c7d04c0aa
maniraj-sivasubbu-pwc <maniraj.sivasubbu-pwc@hpe.com>
2021-11-15 19:01:30 +0530

    g

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.put('/ccs/update', (req, res) => {
 
 
 
-app.delete('/ccs/delete:userId', (req, res) => {
+app.delete('/ccs/delete/:userId', (req, res) => {
     console.log(`CCS delete endpoint triggered for ${userId}`);
     console.log('req.url=======================' + JSON.stringify(req.url));
     console.log('req.headers=======================' + JSON.stringify(req.headers));
